e94ecaa918ce26cc69a9e7686b427f33b694f573
deividfoggi <39340082+deividfoggi@users.noreply.github.com>
2022-11-11 17:39:18 -0300

    Change from app insights config from instrumentation key to connection string.

--- Program.cs
+++ Program.cs
@@ -1,43 +1,53 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.ApplicationInsights.AspNetCore.Extensions;
 
-builder.Logging.ClearProviders();
-builder.Logging.AddConsole();
-builder.Logging.AddAzureWebAppDiagnostics();
-
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-//builder.Services.AddHttpClient();
-builder.Services.AddHttpClient("HttpClientWithSSLUntrusted").ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
+internal class Program
 {
-    ClientCertificateOptions = ClientCertificateOption.Manual,
-    ServerCertificateCustomValidationCallback =
-    (httpRequestMessage, cert, cetChain, policyErrors) =>
+    private static void Main(string[] args)
     {
-        return true;
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Logging.ClearProviders();
+        builder.Logging.AddConsole();
+        builder.Logging.AddAzureWebAppDiagnostics();
+
+
+        // Add services to the container.
+        builder.Services.AddControllersWithViews();
+        //builder.Services.AddHttpClient();
+        builder.Services.AddHttpClient("HttpClientWithSSLUntrusted").ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
+        {
+            ClientCertificateOptions = ClientCertificateOption.Manual,
+            ServerCertificateCustomValidationCallback =
+            (httpRequestMessage, cert, cetChain, policyErrors) =>
+            {
+                return true;
+            }
+        });
+
+        var options = new ApplicationInsightsServiceOptions { ConnectionString = "InstrumentationKey=0a4c9333-63ee-4304-aa04-5fc10a92951d;IngestionEndpoint=https://brazilsouth-1.in.applicationinsights.azure.com/;LiveEndpoint=https://brazilsouth.livediagnostics.monitor.azure.com/" };
+        builder.Services.AddApplicationInsightsTelemetry(options: options);
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        app.Run();
     }
-});
-builder.Services.AddApplicationInsightsTelemetry(builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"]);
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
